afc2100674420c970f7ae5762adf2d4826a094e2
Ataiks <39671990+Ataiks@users.noreply.github.com>
2020-09-23 17:26:56 +0100

    Update phonebook_revamped.py

--- Beginners/phonebook_revamped.py
+++ Beginners/phonebook_revamped.py
@@ -2,14 +2,11 @@
 
 from pycontact_module import *
 
-#Define a function to welcome the user and provide options on how the phonebook works
 
-
-
-            
+#Define a function to initiate the phonebook program
+#Call the menu function from pycontact
 def Phonebook():
-        #initiate a while loop to continuously run the phonebook program
-    #while True:
+            
         #call menu function.
     contacts={}
     menu()
